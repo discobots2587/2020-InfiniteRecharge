c672ef5fef21419bbbec2124c1a7e79c43c4b76b
Rahul Menon <menonrahul02@gmail.com>
2020-01-25 12:59:13 -0600

    Create DriveTrain subsystem class

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+  private Victor left = new Victor(0);
+  private Victor right = new Victor(1);
+  private DifferentialDrive drive = new DifferentialDrive(left, right);
+
+  /**
+   * Creates a new DriveTrain.
+   */
+  public DriveTrain() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public DifferentialDrive getDrive() {
+    return drive;
+  }
+}

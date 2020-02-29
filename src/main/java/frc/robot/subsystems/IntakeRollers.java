4c23d52b68d2e6795b320cab227ec6d6ffc8356b
Rahul Menon <menonrahul02@gmail.com>
2020-02-29 10:28:05 -0600

    Create IntakeRollers subsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeRollers.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeRollersConstants;
+
+public class IntakeRollers extends SubsystemBase {
+  private TalonSRX rollers = new TalonSRX(IntakeRollersConstants.kRollersID);
+
+  /**
+   * Creates a new IntakeRollers.
+   */
+  public IntakeRollers() {
+    rollers.setNeutralMode(NeutralMode.Brake);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
